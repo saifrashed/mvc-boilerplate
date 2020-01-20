94a79e1198e999c3308afaf37d7e68a308f6f527
Saif <saifeddinerashed@icloud.com>
2020-01-20 17:43:28 +0100

    Refactor

--- controller/router.php
+++ controller/router.php
@@ -1,62 +1,61 @@
 <?php
 
-/**
- * Application router class determines use of controller
- */
-class Router {
-
-    public function __construct() {
-        $url     = $_SERVER['REQUEST_URI'];
-        $packets = explode('/', $url);
-        $this->determineDestination($packets);
-    }
-
     /**
-     * Determines location from explodes URL
-     *
-     * @param  string $packets
-     * @return
+     * Application router class determines use of controller
      */
-    public function determineDestination($packets = '') {
+    class Router {
 
-        if (is_array($packets)) {
+        public function __construct() {
+            $url     = $_SERVER['REQUEST_URI'];
+            $packets = explode('/', $url);
+            $this->determineDestination($packets);
+        }
 
-            $packetClass = $packets[4];
-            $packetMethod     = $packets[5];
+        /**
+         * Determines location from explodes URL
+         *
+         * @param  string $packets
+         * @return
+         */
+        public function determineDestination($packets = '') {
 
-            // Fallback class when none are given.
-            (!$packetClass) ? $class = 'home' : $class = $packetClass;
+            if (is_array($packets)) {
 
-            // Fallback method when none are given.
-            (!$packetMethod) ? $method = 'index' : $method = $packetMethod;
+                $packetClass  = $packets[4];
+                $packetMethod = $packets[5];
 
-            // Offset the packets until parameters are given.
-            $params = array_slice($packets, 6);
+                // Fallback class when none are given.
+                (!$packetClass) ? $class = 'home' : $class = $packetClass;
 
-            $this->sendToDestination($class, $method, $params);
+                // Fallback method when none are given.
+                (!$packetMethod) ? $method = 'index' : $method = $packetMethod;
+
+                // Offset the packets until parameters are given.
+                $params = array_slice($packets, 6);
+
+                $this->sendToDestination($class, $method, $params);
+            }
+        }
+
+        /**
+         * Sends to determined location
+         *
+         * @param  $classname
+         * @param  $method
+         * @param  $params
+         */
+        public function sendToDestination($classname, $method, $params) {
+            $class = APP_DIR . '/controller/' . $classname . '.php';
+            require_once($class);
+            // Create object and call method
+            $obj = new $classname;
+
+            if ($method) {
+                die(call_user_func_array(array($obj, $method), $params));
+            }
+        }
+
+        public function __destruct() {
+            # code...
         }
     }
-
-    /**
-     *
-     *
-     * @param  [type] $classname [description]
-     * @param  [type] $method    [description]
-     * @param  [type] $params    [description]
-     * @return [type]            [description]
-     */
-    public function sendToDestination($classname, $method, $params) {
-        $class = APP_DIR . '/controller/' . $classname . '.php';
-        require_once($class);
-        // Create object and call method
-        $obj = new $classname;
-
-        if ($method) {
-            die(call_user_func_array(array($obj, $method), $params));
-        }
-    }
-
-    public function __destruct() {
-        # code...
-    }
-}
